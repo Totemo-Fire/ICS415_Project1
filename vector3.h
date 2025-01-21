95a78c06d70129a796e9309b5d6d9cd3b5147c0e
Mohammed <m7med2003gh@gmail.com>
2025-01-21 21:37:19 +0300

    Diffusion Lighting, added data driven approach to scene definition.

--- vector3.h
+++ vector3.h
@@ -30,6 +30,10 @@ public:
 	{
 		return *this *= 1 / k;
 	}
+
+	double magnitude() {
+		return sqrt(v[0]*v[0] + v[1]*v[1] + v[2]*v[2]);
+	}
 };
 
 Vector3 operator+(const Vector3 &u, const Vector3 &w)
@@ -48,5 +52,14 @@ Vector3 cross(const Vector3 &u, const Vector3 &w)
 				   u.v[2] * w.v[0] - u.v[0] * w.v[2],
 				   u.v[0] * w.v[1] - u.v[1] * w.v[0]);
 }
+
+Vector3 operator*(const Vector3& u, const double k) {
+    return Vector3(u.v[0] * k, u.v[1] * k, u.v[2] * k);
+}
+
+Vector3 operator/(const Vector3& u, const double k) {
+    return Vector3(u.v[0] / k, u.v[1] / k, u.v[2] / k);
+}
+
 
 #endif
